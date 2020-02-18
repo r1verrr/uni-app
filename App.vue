c0118140382799795323c9de4a8c3d55900c99c2
Song Miaoqiang <739026055@qq.com>
2020-02-18 11:06:44 +0800

    first commit

---
+++ App.vue
@@ -0,0 +1,22 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	@import './common/uni.css';
+	/* 引入自定义图标库 */
+	@import './common/icon.css';
+	
+	
+</style>
