c72c14cb4b0803309e5ae1d124e169c8f4907880
Song Miaoqiang <739026055@qq.com>
2020-02-21 15:48:26 +0800

    封装列表样式组件

---
+++ components/index/index-list.vue
@@ -0,0 +1,115 @@
+<template>
+	<view class="index-list">
+		<view class="index-list1 u-f-ac u-f-jsb">
+			<view class="u-f-ac">
+				<image :src="item.userpic" mode="widthFix" lazy-load></image>
+				{{item.username}}
+			</view>
+			<view class="u-f-ac" v-show="item.isguanzhu">
+				<view class="icon iconfont icon-zengjia"></view>关注
+			</view>	
+		</view>
+		<view class="index-list2">{{item.title}}</view>
+		<view class="index-list3 u-f-ajc">
+			<!-- 图片 -->
+			<image :src="item.titlepic" mode="widthFix" lazy-load></image>
+			<!-- 视频 -->
+			<template v-if="item.type == 'video'">
+				<view class="index-list-play icon iconfont icon-bofang"></view>
+					<view class="index-list-playinfo">
+						{{item.playnum}}  {{item.long}}
+					</view>
+		
+			</template>
+					</view>
+		<view class="index-list4 u-f-ac u-f-jsb">
+			<view class="u-f-ac">
+				<view class="u-f-ac " :class="{'acitve':(item.infonum.index == 1)}" ><view class="icon iconfont icon-icon_xiaolian-mian"></view >{{item.infonum.dingnum}}</view>
+				<view class="u-f-ac" :class="{'acitve':(item.infonum.index == 2)}"><view class="icon iconfont icon-kulian"></view>{{item.infonum.cainum}}</view>
+			</view>
+			<view class="u-f-ac">
+				<view class="u-f-ac"><view class="icon iconfont icon-pinglun1"></view >{{item.commentnum}}</view>
+				<view class="u-f-ac"><view class="icon iconfont icon-zhuanfa"></view>{{item.sharenum}}</view>
+			</view>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default{
+		props:{
+			item:Object,
+			index:Number
+		}
+	}
+</script>
+
+<style scoped>
+	.index-list{
+		padding: 20upx;
+		border-bottom: 1upx solid #EEEEEE; 
+		
+	}
+	.index-list1>view:first-child{
+		color: #f8f2f7;
+		}
+		.index-list1>view:first-child image{
+			width: 80upx;
+			height: 80upx;
+			border-radius: 100%;
+			margin-right: 10upx; 
+		}
+		.index-list1>view:last-child{
+		background: #F4F4F4;
+		border-radius: 5upx;
+		padding: 0 10upx;
+		
+		}
+		.index-list1>view:last-child>view{
+		
+		}
+	.index-list2{
+		padding-top: 15upx;
+		font-size: 32upx;
+	}
+	.index-list3{
+			padding-top: 15upx;
+			position: relative;
+	}
+	.index-list3>image{
+		width: 100%;
+		border-radius: 20upx;
+	}
+	.index-list4 view{
+		color: #999999;
+	}
+	.index-list4{
+		padding: 15upx 0;
+	}
+	.index-list4>view>view>view{
+		margin-right: 10upx;
+	}
+	.index-list4>view>view:first-child{
+		margin-right: 10upx;
+	}
+	.index-list-play{
+		position: absolute;
+		font-size: 80upx;
+		color: #FFFFFF;
+		
+	}
+	.index-list-playinfo{
+		position: absolute;
+		background: #555b40;
+		color: #FFFFFF;
+		bottom: 8upx;
+		right: 8upx;
+		border-radius: 40upx;
+		font-size: 25upx;
+		padding: 0 10upx;
+		}
+		.index-list4 .acitve,.index-list4 .acitve>view{
+			color: #c8e323;
+		}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,46 +1,55 @@
 <template>
 	<view >
+		<block v-for="(item,index) in list" :key="index">
+			<index-list :item="item" :index="index"></index-list>
+		</block>
 		
-		<view class="index-list">
-			<view class="index-list1 u-f-ac u-f-jsb">
-				<view class="u-f-ac">
-					<image src="../../static/demo/userpic/12.jpg" mode="widthFix" lazy-load></image>
-					昵称
-				</view>
-				<view class="u-f-ac">
-					<view class="icon iconfont icon-zengjia"></view>关注
-				</view>	
-			</view>
-			<view class="index-list2">我是标题</view>
-			<view class="index-list3 u-f-ajc">
-				<!-- 图片 -->
-				<image src="../../static/demo/datapic/11.jpg" mode="widthFix" lazy-load></image>
-				<!-- 视频 -->
-				<view class="index-list-play icon iconfont icon-bofang"></view>
-				<view class="index-list-playinfo">
-					25w次播放  2:47
-				</view>
-			</view>
-			<view class="index-list4 u-f-ac u-f-jsb">
-				<view class="u-f-ac">
-					<view class="u-f-ac" ><view class="icon iconfont icon-icon_xiaolian-mian"></view >10</view>
-					<view class="u-f-ac"><view class="icon iconfont icon-kulian"></view>10</view>
-				</view>
-				<view class="u-f-ac">
-					<view class="u-f-ac"><view class="icon iconfont icon-pinglun1"></view >10</view>
-					<view class="u-f-ac"><view class="icon iconfont icon-zhuanfa"></view>10</view>
-				</view>
-			</view>
-			
-		</view>
 		
 	</view>
 </template>
 
 <script>
+	import indexList from "../../components/index/index-list.vue"
 	export default {
 		data() {
 			return {
+					list:[
+						{
+							userpic:"../../static/demo/userpic/12.jpg",
+							username:"昵称",
+							isguanzhu:false,
+							title:"我是标题",
+							type:"img", //img:图文 video:视频
+							titlepic:"../../static/demo/datapic/11.jpg",
+							infonum:{
+								index:0,// 0:没有操作 1:顶 2:踩
+								dingnum:11,
+								cainum:11
+							},
+							
+							commentnum:10,
+							sharenum:10
+							
+						},{
+							userpic:"../../static/demo/userpic/12.jpg",
+							username:"昵称",
+							isguanzhu:true,
+							title:"我是标题",
+							type:"img", //img:图文 video:视频
+							titlepic:"../../static/demo/datapic/11.jpg",
+							playnum:"20w",
+							long:"2:47",
+							infonum:{
+								index:1,// 0:没有操作 1:顶 2:踩
+								dingnum:11,
+								cainum:11
+							},
+							
+							commentnum:10,
+							sharenum:10
+							
+						},
+						]
 					
 			}
 		},
@@ -49,72 +58,13 @@
 		},
 		methods: {
 
+		},
+		components:{
+			indexList
 		}
 	}
 </script>
 
 <style>
-	.index-list{
-		padding: 20upx;
-		border-bottom: 1upx solid #EEEEEE; 
-		
-	}
-	.index-list1>view:first-child{
-		color: #f8f2f7;
-		}
-		.index-list1>view:first-child image{
-			width: 80upx;
-			height: 80upx;
-			border-radius: 100%;
-			margin-right: 10upx; 
-		}
-		.index-list1>view:last-child{
-		background: #F4F4F4;
-		border-radius: 5upx;
-		padding: 0 10upx;
-		
-		}
-		.index-list1>view:last-child>view{
-		
-		}
-	.index-list2{
-		padding-top: 15upx;
-		font-size: 32upx;
-	}
-	.index-list3{
-			padding-top: 15upx;
-			position: relative;
-	}
-	.index-list3>image{
-		width: 100%;
-		border-radius: 20upx;
-	}
-	.index-list4 view{
-		color: #999999;
-	}
-	.index-list4{
-		padding: 15upx 0;
-	}
-	.index-list4>view>view>view{
-		margin-right: 10upx;
-	}
-	.index-list4>view>view:first-child{
-		margin-right: 10upx;
-	}
-	.index-list-play{
-		position: absolute;
-		font-size: 80upx;
-		color: #FFFFFF;
-		
-	}
-	.index-list-playinfo{
-		position: absolute;
-		background: #555b40;
-		color: #FFFFFF;
-		bottom: 8upx;
-		right: 8upx;
-		border-radius: 40upx;
-		font-size: 25upx;
-		padding: 0 10upx;
-		}
+	
 </style>
