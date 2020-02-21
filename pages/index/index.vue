403eddb7e684e82462fdf98c0b602baa3d173a71
Song Miaoqiang <739026055@qq.com>
2020-02-21 23:27:17 +0800

    滚动tab导航

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,31 @@
 <template>
 	<view >
-		<block v-for="(item,index) in list" :key="index">
-			<index-list :item="item" :index="index"></index-list>
-		</block>
 		
+		<view class="uni-tab-bar">
+			<scroll-view scroll-x class="uni-swiper-tab">
+				<block v-for="(tab,index) in tabBars" :key="tab.id">
+					<view class="swiper-tab-list" 
+					      :class="{'active':tabIndex == index}"
+								@tap="tabtap(index)">
+						{{tab.name}}
+						<view class="swiper-tab-line"></view>
+					</view>
+				</block>
+			</scroll-view>
+		</view>
+		
+		<view class="uni-tab-bar">
+		<swiper class="swiper-box" :style="{height:swiperheight+'px'}" :current="tabIndex" @change="tabChange">
+			<swiper-item v-for="(items,index) in newslist" :key="index">
+				<scroll-view scroll-y class="list">
+					<block v-for="(item,index1) in items.list" :key="index1">
+						<index-list :item="item" :index="index1"></index-list>
+					</block> 
+				</scroll-view>
+				
+			</swiper-item>
+		</swiper>
+		</view>
 		
 	</view>
 </template>
@@ -13,8 +35,20 @@
 	export default {
 		data() {
 			return {
-					list:[
-						{
+				swiperheight:500,
+				tabIndex:0,
+				tabBars:[
+					{name:"关注",id:"guanzhu"},
+					{name:"推荐",id:"tuijian"},
+					{name:"体育",id:"tiyu"},
+					{name:"热点",id:"redian"},
+					{name:"财经",id:"caijing"},
+					{name:"娱乐",id:"yule"},
+				],
+				newslist:[
+					{
+						list:[
+							{
 							userpic:"../../static/demo/userpic/12.jpg",
 							username:"昵称",
 							isguanzhu:false,
@@ -50,14 +84,116 @@
 							
 						},
 						]
+					},{
+						list:[
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:false,
+								title:"我是标题",
+								type:"img", //img:图文 video:视频
+								titlepic:"../../static/demo/datapic/11.jpg",
+								infonum:{
+									index:0,// 0:没有操作 1:顶 2:踩
+									dingnum:11,
+									cainum:11
+								},
+								
+								commentnum:10,
+								sharenum:10
+								
+							},{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:true,
+								title:"我是标题",
+								type:"img", //img:图文 video:视频
+								titlepic:"../../static/demo/datapic/11.jpg",
+								playnum:"20w",
+								long:"2:47",
+								infonum:{
+									index:1,// 0:没有操作 1:顶 2:踩
+									dingnum:11,
+									cainum:11
+								},
+								
+								commentnum:10,
+								sharenum:10
+								
+							},
+						]
+					},{
+						list:[
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:false,
+								title:"我是标题",
+								type:"img", //img:图文 video:视频
+								titlepic:"../../static/demo/datapic/11.jpg",
+								infonum:{
+									index:0,// 0:没有操作 1:顶 2:踩
+									dingnum:11,
+									cainum:11
+								},
+								
+								commentnum:10,
+								sharenum:10
+								
+							},{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:true,
+								title:"我是标题",
+								type:"img", //img:图文 video:视频
+								titlepic:"../../static/demo/datapic/11.jpg",
+								playnum:"20w",
+								long:"2:47",
+								infonum:{
+									index:1,// 0:没有操作 1:顶 2:踩
+									dingnum:11,
+									cainum:11
+								},
+								
+								commentnum:10,
+								sharenum:10
+								
+							},
+						]
+					},{
+						list:[]
+					},{
+						list:[]
+					},{
+						list:[]
+					},{
+						list:[]
+					},
+				],
+					list:[
+						
+						]
+						
 					
 			}
 		},
 		onLoad() {
-
+			uni.getSystemInfo({
+				success:(res) =>{
+					let height = res.windowHeight-uni.upx2px(100)
+					this.swiperheight = height;
+				}
+			})
 		},
 		methods: {
-
+			// 点击事件
+			tabtap(index){
+				this.tabIndex = index;
+			},
+			// 滑动事件
+			tabChange(e){
+				this.tabIndex= e.detail.current;
+			}
 		},
 		components:{
 			indexList
@@ -65,6 +201,24 @@
 	}
 </script>
 
-<style>
+<style >
+	.uni-swiper-tab{
+		border-bottom: 1upx solid #EEEEEE;
+		
+	}
+	.swiper-tab-list{
+		color: #989798;
+		font-weight: bold;
+	}
+	.uni-tab-bar .active{
+		color: #1d1c1d;
+	}
+	.active .swiper-tab-line{
+		border-bottom: 6upx solid #ece579;
+		width: 70upx;
+		margin: auto;
+		border-top: 6upx solid #FEDE33;
+		border-radius: 20upx;	
+	}
 	
 </style>
