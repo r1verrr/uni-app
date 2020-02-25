4f79424e6157f1dc8342af25b91729c10aae33d7
Song Miaoqiang <739026055@qq.com>
2020-02-25 10:59:06 +0800

    上传多图功能

--- pages/add-input/add-input.vue
+++ pages/add-input/add-input.vue
@@ -3,24 +3,37 @@
 		<!-- 自定义导航栏 -->
 		<uni-nav-bar :statusBar="true" rightText="发布" left-icon="back" @click-left="back" @click-right="submit">
 			<view class="u-f-ajc" @tap="changelook">
-				所有人可见
+				{{yinsi}}
 				<view class="icon iconfont icon-xialazhankai"></view>
 			</view>
 		</uni-nav-bar>
+		
+		<!-- 多行输入框 -->
+		<view class="uni-textarea">
+			<textarea v-model="text" placeholder="说一句话吧" />
+		</view>
+		
+		<!-- 上传多图 -->
+		<uploudImages @uploud="uploud"></uploudImages>
 	</view>
 </template>
 
 <script>
+	let changelook=['所有人可见','仅自己可见']
 		import uniNavBar from "../../components/uni-nav-bar/uni-nav-bar.vue";
+		import uploudImages from "../../components/common/uploud-images.vue";
 		
 	export default {
 		data() {
 			return {
-				
+				yinsi:"所有人可见",
+				text:"1111",
+				imglist:[]
 			}
 		},
 		components:{
-			uniNavBar
+			uniNavBar,
+			uploudImages
 		},
 		methods: {
 			// 返回
@@ -36,7 +49,17 @@
 			// 隐私
 			changelook(){
 				console.log("隐私")
+				uni.showActionSheet({
+					itemList:changelook,
+					success: (res) => {
+						this.yinsi = changelook[res.tapIndex]
+					}
+				})
+			},
+			uploud(arr){
+				this.imglist=arr
 			}
+			
 			
 		},
 		onNavigationBarButtonTap(e){
@@ -53,5 +76,7 @@
 </script>
 
 <style>
-
+ .uni-textarea{
+	 border: 1upx solid #EEEEEE;
+ }
 </style>
